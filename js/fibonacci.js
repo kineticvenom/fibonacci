8127fec354f7c995ed8dba2e74ad6d494fe6032a
kineticvenom <kalebvarnes@yahoo.com>
2022-05-25 09:23:05 -1000

    Code Added

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,14 @@
 const fibonacci = (num) => {
-    
+    let num1=0;
+    let num2=1;
+    let sum=0;
+
+      for(i=0;i<num;i++){
+        sum = num1 +num2;
+        num1 = num2;
+        num2 = sum;
+      }
+      return num1    
 }
 
 module.exports = {fibonacci}
