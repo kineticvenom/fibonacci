31f64ac55923cf76ab2a5125d43c006322b6a1dc
kineticvenom <kalebvarnes@yahoo.com>
2022-05-25 12:08:45 -1000

    New Code

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,19 @@
 def fibonacci(n):
-  pass
+  num1 = 0
+  num2 = 1
+  sum = 0
+
+  if n < 1 : 
+    return 1
+
+  i=0
+  for i in range(0,n) :
+    sum = num1 + num2
+    num1 = num2
+    num2 = sum 
+    i+1
+   
+
+  return num1    
 
 
